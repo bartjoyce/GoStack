0426fb7a0d769b7c142d813361406f10e16694d1
bartjoyce <bartholomew.michael.joyce@gmail.com>
2014-06-07 23:16:49 +0100

    Compiled gostack.js

--- bin/gostack.js
+++ bin/gostack.js
@@ -137,6 +137,34 @@
 
 })();
 
+/**
+ * DOM.js implements:
+ * Element.addEventListener()
+ */
+(function() {
+
+  // Store original functions
+  var addEventListener = Element.prototype.addEventListener;
+  var removeEventListener = Element.prototype.removeEventListener;
+
+  var decorateAndCache = function decorateAndCache(callback) {
+    return (callback.__decorated__ = go.decorate(callback));
+  }
+
+  Element.prototype.addEventListener = function(event, callback, capture) {
+    callback = decorateAndCache(callback);
+
+    return addEventListener.call(this, event, callback, capture);
+  }
+
+  Element.prototype.removeEventListener = function(event, callback, capture) {
+    callback = callback.__decorated__ || callback;
+
+    return removeEventListener.call(this, event, callback, capture);
+  }
+
+})();
+
 
 })(window);
 
